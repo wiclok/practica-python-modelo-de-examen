dc02a1fe7fcbff0c4ececc1f079c029613eaf53a
brian Céspedes aguilera <cespedes.aguilera.brian@gmail.com>
2024-05-01 17:29:23 -0300

    add function sorted_by_age

--- app.py
+++ app.py
@@ -10,13 +10,28 @@ def import_data_over_25_years(archivo):
         cabecera = next(lector)
         age_and_fullname = []
         for fila in lector:
-            age_and_fullname.append(fila)
-        print(age_and_fullname)
-        print(cabecera)
+            age = fecha_actual - int(fila[0].split(',')[0].split('/')[2])
+            name = fila[0].split(',')[1]
+            age_of_birthday = fila[0].split(',')[0]
+            age_and_fullname.append([age, name, age_of_birthday])
+        
+        over_of_25_years = []
+        for person in age_and_fullname:
+            if person[0] >= 25:
+                over_of_25_years.append(person)
+        
+        return over_of_25_years
+
+def order_by_age(over_of_25_years):
+    sorted_by_age = sorted(over_of_25_years, key=lambda x: x[0])
+    return sorted_by_age
+
 
 def ejecutar_programa():
-    archivo = 'D:\Practica\Practica-Modelo-Examen-Python\edades.csv'
-    import_data_over_25_years(archivo)
+    archivo = 'D:\\Practica\\Practica-Modelo-Examen-Python\\edades.csv'
+    over_of_25_years = import_data_over_25_years(archivo)
+    
+    print(order_by_age(over_of_25_years))
 
 
 if __name__ == '__main__':
