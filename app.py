8bd0e41ea9498f9fd43581cd978f7bdd3ed897bc
brian Céspedes aguilera <cespedes.aguilera.brian@gmail.com>
2024-05-02 21:01:06 -0300

    adding function DataFrame with columns Age and Frequency

--- app.py
+++ app.py
@@ -46,16 +46,35 @@ def frequency_of_age(sorted_by_age):
     
     return frequency
 
+def generate_DataFrame(frequency):
+    edades = []
+    for edad in frequency:
+        edades.append(edad)
+    
+    fi = {}
+    
+    for i in edades:
+        if i in fi:
+            fi[i] += frequency[i]+1
+        else:
+            fi[i] = frequency[i]
+    
+    edades_keys = fi.keys()
+    edades_values = fi.values()
+    df = pd.DataFrame({'Edades': edades_keys, 'fi': edades_values})
+    return df
+
 def ejecutar_programa():
     archivo = 'D:\\Practica\\Practica-Modelo-Examen-Python\\edades.csv'
     over_of_25_years = import_data_over_25_years(archivo)
     
     sorted_by_age = order_by_age(over_of_25_years)
-
+    
     count_age_different = age_different(sorted_by_age)
     
     frequency = frequency_of_age(sorted_by_age)
-
+    
+    DataFrame = generate_DataFrame(frequency)
 
 if __name__ == '__main__':
     ejecutar_programa()
